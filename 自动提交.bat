233b0370258074b50cf718b74418e01678c54fb5
huangWI <760474223@qq.com>
2023-11-14 14:33:09 +0800

    2023年11月14日

--- 自动提交.bat
+++ 自动提交.bat
@@ -1,6 +1,6 @@
+@echo off
+@title bat 交互执行git命令
 E:
-E:\D盘文件\WanLi.Huang\00工作计划\Working
+cd E:\D盘文件\WanLi.Huang\00工作计划\Working
 git add .
-git status
-git commit -m "自动推送PC"
-git push
+git commit -m %date:~0,4%年%date:~5,2%月%date:~8,2%日
